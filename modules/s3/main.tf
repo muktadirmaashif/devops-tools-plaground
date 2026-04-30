a91e8e046edceca8852145793d78051a162357e1
Muktadir M Aashif <muktadirmaashif@gmail.com>
2026-04-30 13:12:11 +0600

    implement module

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,1 +1,6 @@
 data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+locals {
+  s3_name = format("tf-pg-%s", data.aws_caller_identity.current.id)
+}
--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -1,1 +1,13 @@
-output "
+output "current_caller" {
+  value = data.aws_caller_identity.current
+}
+
+output "current_region" {
+  value = data.aws_region.current
+
+}
+
+output "bucket_name" {
+
+  value = local.s3_name
+}
