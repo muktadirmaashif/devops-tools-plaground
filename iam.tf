3d48b11d5ceff9a56112beb9ed038a13fa986e87
Muktadir M Aashif <muktadirmaashif@gmail.com>
2026-04-30 10:39:58 +0600

    y

--- iam.tf
+++ iam.tf
@@ -6,19 +6,3 @@ data "aws_iam_group" "devops" {
   group_name = "devops"
 }
 
-locals {
-  user_in_devops = contains(
-    data.aws_iam_group.devops.users,
-    data.aws_iam_user.muktadir.user_name
-  )
-}
-
-resource "null_resource" "assert_user_in_group" {
-  lifecycle {
-    precondition {
-      condition     = local.user_in_devops
-      error_message = "User is NOT a member of the devops group"
-    }
-  }
-}
-
